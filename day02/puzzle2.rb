a8811fabdfe18064402f9862c65ea56716f154cb
Jean Tessier <jean@jeantessier.com>
2025-12-02 21:54:06 -0800

    Optimize by removing floating point arithmetic
    Use `x % n == 0` to find if `x` is a multiple of `n`, instead of doing
    floating point division.  I monkey patched the logic into a
    `#multiple_of?` method on `Integer` to make the code easier to read.
    
    The performance gains are minimal, but readability is great.

--- day02/puzzle2.rb
+++ day02/puzzle2.rb
@@ -4,11 +4,17 @@
 
 INVALID_ID_REGEX = /^(\d+)\1$/
 
+class Integer
+  def multiple_of?(number)
+    self % number == 0
+  end
+end
+
 # lines = readlines
 # lines = File.readlines('sample.txt', chomp: true)
 # ANSWER = 4174379265 # (in 40 ms)
 lines = File.readlines('input.txt', chomp: true)
-ANSWER = 58961152806 # (in 10,649 ms)
+ANSWER = 58961152806 # (in 10,293 ms)
 
 ranges = lines
   .map { |line| line.split(',') }
@@ -27,7 +33,7 @@ invalid_ids = ranges
       .select do |id|
         digits = id.to_s
         (1..(digits.size >> 1))
-          .select { |n| (digits.size / n) == (digits.size / (n.to_f)) }
+          .select { |n| digits.size.multiple_of?(n) }
           .map { |n| digits[0...n] }
           .any? { |prefix| digits =~ /^(#{prefix})+$/ }
       end
