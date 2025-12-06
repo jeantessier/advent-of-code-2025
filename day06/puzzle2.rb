e48d8bfd69630d037486a9306bcbf699a2304caf
Jean Tessier <jean@jeantessier.com>
2025-12-05 23:00:29 -0800

    Replace globals variables with call to #chunk

--- day06/puzzle2.rb
+++ day06/puzzle2.rb
@@ -6,7 +6,7 @@
 # lines = File.readlines('sample.txt', chomp: true)
 # ANSWER = 3263827 # (in 50 ms)
 lines = File.readlines('input.txt', chomp: true)
-ANSWER = 9077004354241 # (in 66 ms)
+ANSWER = 9077004354241 # (in 71 ms)
 
 worksheet = lines
   .map { |line| line.split '' }
@@ -17,19 +17,13 @@ puts '---------'
 puts worksheet.map(&:inspect)
 puts
 
-problems = []
-current_problem = []
-
-worksheet.each do |row|
-  if row.all? { |c| c == ' ' }
-    problems << current_problem
-    current_problem = []
-  else
-    current_problem << row[-1] if row[-1] != ' '
-    current_problem << row[..-2].join.to_i
+problems = worksheet
+  .chunk { |row| row.all? { |c| c == ' ' } } # group into "paragraphs" separated by blank lines
+  .filter { |pair| !pair[0] } # remove separators
+  .map { |pair| pair[1] } # keep only the paragraphs themselves
+  .map do |problem| # convert each paragraph into a problem: operation and numbers
+    [problem[0].last] + problem.map { |number| number[..-2] }.map(&:join).map(&:to_i)
   end
-end
-problems << current_problem
 
 puts 'Problems'
 puts '--------'
