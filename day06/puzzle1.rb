8fd400938bc48529db8225599261c3577ef42f4e
Jean Tessier <jean@jeantessier.com>
2025-12-05 22:06:04 -0800

    Fix typo

--- day06/puzzle1.rb
+++ day06/puzzle1.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# Login to https://adventofcode.com/2025/day/5/input to download 'input.txt'.
+# Login to https://adventofcode.com/2025/day/6/input to download 'input.txt'.
 
 # lines = readlines
 # lines = File.readlines('sample.txt', chomp: true)
