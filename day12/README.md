72bf4138252148d3e38cc4767068deeb836dd078
Jean Tessier <jean@jeantessier.com>
2025-12-11 22:00:34 -0800

    Day 12 Puzzle 1 (WIP)

---
+++ day12/README.md
@@ -0,0 +1,11 @@
+# [Day 12](https://adventofcode.com/2025/day/12)
+
+## Puzzle 1
+
+I have no clue.
+
+I wrote code to parse the input, but I'm drawing a blank on how to solve it.  I
+feel like I need to try each permutation of shapes, and for each shape, try each
+orientation and flip (up to 8 variations).  Some geometries cut down on these
+variations.  For example, an `H` shape is the same when flipped and when rotated
+180&deg;, so it has only two variations.
---
+++ day12/puzzle1.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+# Login to https://adventofcode.com/2025/day/12/input to download 'input.txt'.
+
+SHAPE_REGEX = /^[#.]+$/
+REGION_REGEX = /^(?<width>\d+)x(?<length>\d+): (?<quantities>.+)$/
+
+class Region
+  attr_reader :width, :length, :quantities
+
+  def initialize(width, length, quantities)
+     @width = width.to_i
+     @length = length.to_i
+     @quantities = quantities.split.map(&:to_i)
+  end
+end
+
+# lines = readlines
+lines = File.readlines('sample.txt', chomp: true)
+ANSWER = 2 # (in ?? ms)
+# lines = File.readlines('input.txt', chomp: true)
+# ANSWER = -1 # (in ?? ms)
+
+shapes = []
+current_shape = []
+
+lines.each do |line|
+  case line
+  when SHAPE_REGEX
+    current_shape << line
+  when ''
+    shapes << current_shape
+    current_shape = []
+  end
+end
+
+puts "Shapes (#{shapes.size})"
+puts '------'
+puts shapes.map { |shape| shape.join("\n") }.join("\n\n")
+puts
+
+regions = lines
+  .map { |line| line.match REGION_REGEX }
+  .select { |match| match }
+  .map { |match| Region.new(match[:width], match[:length], match[:quantities]) }
+
+puts "Regions (#{regions.size})"
+puts '-------'
+puts regions.map(&:inspect)
+puts
+
+# total = paths.size
+#
+# puts "Total: #{total}"
+# puts "OK" if total == ANSWER
